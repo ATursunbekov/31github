bdf27d6143a220cd021e31fdf25a84b66ba744fc
Alikhan <alikhan.tursunbekov@gmail.com>
2023-11-24 22:38:54 +0600

    Solve Leetcode problems

---
+++ 31github/Problems/12.swift
@@ -0,0 +1,29 @@
+//
+//  12.swift
+//  31github
+//
+//  Created by Alikhan Tursunbekov on 24/11/23.
+//
+
+import Foundation
+
+class Solution12 {
+    
+    private let values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    private let symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    
+    func intToRoman(_ num: Int) -> String {
+        
+        var int = num
+        var sym = ""
+        
+        while int > 0 {
+            for (i, d) in values.enumerated() where int - d >= 0 {
+                int -= d
+                sym += symbols[i]
+                break
+            }
+        }
+        return sym
+    }
+}

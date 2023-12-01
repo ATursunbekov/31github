ba5e34ea005d1536c9765ab882155a8f1a49acff
Alikhan <alikhan.tursunbekov@gmail.com>
2023-12-01 11:47:07 +0600

    Solve Leetcode problems

---
+++ 31github/Problems/125.swift
@@ -0,0 +1,17 @@
+extension String {
+    var alphanumeric: String {
+        return self.components(separatedBy: CharacterSet.alphanumerics.inverted).joined()
+    }
+}
+
+class Solution125 {
+    func isPalindrome(_ s: String) -> Bool {
+        var temp = s.lowercased()
+        temp.removeAll { val in
+        val == " "
+        }
+        temp = temp.alphanumeric
+        var temp1 = String(temp.reversed())
+        return temp == temp1
+    }
+}
--- 31github/main.swift
+++ 31github/main.swift
@@ -7,8 +7,40 @@
 
 import Foundation
 
+//func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+//    var res: [Int] = []
+//    var index1 = 0
+//    var index2 = 0
+//    for i in 0..<(n + m) {
+//        if  index1 < nums1.count && nums1[index1] <= nums2[index2] {
+//            print("index1: \(index1)")
+//            res.append(nums1[index1])
+//            index1 += 1
+//        } else if index2 < nums2.count {
+//            res.append(nums2[index2])
+//            index2 += 1
+//            print("index2: \(index2)")
+//        }
+//    }
+//    nums1 = res
+//    print(res)
+//}
 
+//var temp = [1, 2, 3, 4]
+//temp.removeAll { num in
+//    num == 0
+//}
+//merge(&temp, 4, [2,4, 5,8], 4)
+extension String {
+    var alphanumeric: String {
+        return self.components(separatedBy: CharacterSet.alphanumerics.inverted).joined()
+    }
+}
 
-var str = "Hello world"
-
-print(round(sqrt(2.0)))
+var temp = "`l;`` 1o1 ??;l`"
+temp = temp.lowercased()
+temp.removeAll { val in
+    val == " "
+}
+temp = temp.alphanumeric
+print(temp)
